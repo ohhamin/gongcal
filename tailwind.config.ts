a12f2b7f1aeb8ed465f27d7476bcba7cef7fe568
Hamin <ohm55545@naver.com>
2026-05-10 22:02:07 +0900

    테일윈드로 다크모드 글자 영향 안받게

---
+++ tailwind.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+    darkMode: 'class',
+
+    content: [
+        './app/**/*.{js,ts,jsx,tsx,mdx}',
+        './pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './components/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+
+    theme: {
+        extend: {
+            colors: {
+                primary: '#111827',
+            },
+        },
+    },
+
+    plugins: [],
+};
+
+export default config;
