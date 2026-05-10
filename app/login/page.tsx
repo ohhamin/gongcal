2f7cbfaf2972ea8b232759809cec92368718339f
Hamin <ohm55545@naver.com>
2026-05-10 21:17:25 +0900

    리다이렉트 주소 변경

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -7,7 +7,7 @@ export default function LoginPage() {
         await supabase.auth.signInWithOAuth({
             provider: 'google',
             options: {
-                redirectTo: 'http://localhost:3000/auth/callback',
+                redirectTo: `${window.location.origin}/auth/callback`,
             },
         });
     };
