4f6c523e33ccc3ab6c9ba8de961c2a84a3f140e8
Hamin <ohm55545@naver.com>
2026-05-10 21:44:14 +0900

    카카오에서 scopes 제한하기

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -17,6 +17,7 @@ export default function LoginPage() {
             provider: 'kakao',
             options: {
                 redirectTo: `${window.location.origin}/auth/callback`,
+                scopes: 'profile_nickname',
             },
         });
     };
